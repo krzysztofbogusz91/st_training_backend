c48d92b72c856fdea185dd601c097780ee4e269a
krzysztofbogusz91 <krzysztof.bogusz@netguru.com>
2020-09-14 20:39:49 +0200

    Saves activity

--- auth/activity/models.py
+++ auth/activity/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from django.conf import settings
 # TODO accept only precise types of activity_type
 class ExerciseSet(models.Model):
-    id = models.IntegerField(primary_key=True)
     weights = models.IntegerField()
     reps = models.IntegerField()
     notes = models.TextField(blank=True)
 class Exercise(models.Model):
     exercise_name = models.CharField(max_length=100)
     sets = models.ManyToManyField(ExerciseSet, related_name='exercise_set', null = True, blank = True,)
-class StrengthSection(models.Model):
-    name = models.TextField(blank=True)
+class StrengthSections(models.Model):
     section_name = models.CharField(max_length=100)
-    exercises = models.ForeignKey('activity.Exercise', related_name='exercise', primary_key=True, null=False,unique=True, on_delete=models.CASCADE)
+    exercises = models.ManyToManyField(Exercise, related_name='exercise', null = True, blank = True,)
 
 class Activity(models.Model):
     activity_type = models.CharField(max_length=30)
     start_date = models.DateField()
+    name = models.TextField(default="text", blank=True)
     description = models.TextField(blank=True)
     posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
-    strength = models.ForeignKey('activity.StrengthSection',primary_key=True, related_name='strength', null=False,unique=True, on_delete=models.CASCADE)
+    strength = models.ManyToManyField(StrengthSections, related_name='exercise', null = True, blank = True,)
--- auth/activity/schema.py
+++ auth/activity/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Activity, StrengthSection, Exercise, ExerciseSet
+from .models import Activity, StrengthSections, Exercise, ExerciseSet
 from users.schema import UserType
 
 class ExerciseSetInput(graphene.InputObjectType):
@@ -11,21 +11,20 @@ class ExerciseSetInput(graphene.InputObjectType):
 class ExerciseSetType(DjangoObjectType):
     class Meta:
         model = ExerciseSet
-class ExerciseInput(graphene.InputObjectType):
+class ExercisesInput(graphene.InputObjectType):
     exercise_name = graphene.String()
     sets = graphene.List(ExerciseSetInput)
 
 class ExerciseType(DjangoObjectType):
     class Meta:
         model = Exercise
-class StrengthSectionInput(graphene.InputObjectType):
-    name = graphene.String()
+class StrengthSectionsInput(graphene.InputObjectType):
     section_name = graphene.String()
-    exercises = ExerciseInput()
+    exercises = graphene.List(ExercisesInput)
 
-class StrengthSectionType(DjangoObjectType):
+class StrengthSectionsType(DjangoObjectType):
     class Meta:
-        model = StrengthSection
+        model = StrengthSections
 class ActivityType(DjangoObjectType):
     class Meta:
         model = Activity
@@ -41,62 +40,72 @@ class CreateActivity(graphene.Mutation):
     activity_type = graphene.String()
     start_date = graphene.Date()
     description = graphene.String()
+    name = graphene.String()
     posted_by = graphene.Field(UserType)
-    strength = graphene.Field(StrengthSectionType)
+    strength = graphene.List(StrengthSectionsType)
 
     class Arguments:
         activity_type = graphene.String()
         start_date = graphene.Date()
+        name = graphene.String()
         description = graphene.String()
-        strength = StrengthSectionInput()
+        strength = graphene.List(StrengthSectionsInput)
 
-    def mutate(self, info, activity_type, start_date, description, strength):
+    def mutate(self, info, activity_type, start_date, name, description, strength):
         user = info.context.user or None
-
-        sets = []
-        for set in strength.exercises.sets:
-            print(set)
-            set_data = ExerciseSet(
-                id=100,
-                weights=set.weights,
-                reps=set.reps,
-                notes=set.notes
-            )
-            set_data.save()
-            sets.append(set_data)
-            
-        exercises = Exercise(
-            exercise_name = strength.exercises.exercise_name,
-        )
-
-        exercises.save()
-
-        exercises.sets.set(sets)
-
-        exercises.save()
-
-        strength_field = StrengthSection(
-            name=strength.name,
-            section_name=strength.section_name,
-            exercises=exercises
-        )
-        strength_field.save()
         
+        sections = []
+        exercises = []
+        sets = []
+        # sections
+        for section in strength:
+            section_data = StrengthSections(
+                section_name=section.section_name
+            )
+            section_data.save()
+            # exercises
+            for exercise in section.exercises:
+                exercise_data = Exercise(
+                    exercise_name=exercise.exercise_name,
+                )
+                exercise_data.save()
+                # sets
+                for exercise_set in exercise.sets:
+                    set_data = ExerciseSet(
+                        weights=exercise_set.weights,
+                        reps=exercise_set.reps,
+                        notes=exercise_set.notes
+                    )
+                    set_data.save()
+                    sets.append(set_data)
+                # set exercise sets
+                exercise_data.sets.set(sets)
+                exercise_data.save()
+                exercises.append(exercise_data)
+            # set section exercises
+            section_data.exercises.set(exercises)
+            section_data.save()
+            # append to main array
+            sections.append(section_data)
+        # end section loop
         activity = Activity(
             activity_type=activity_type,
             start_date=start_date,
+            name=name,
             description=description,
             posted_by=user,
-            strength=strength_field,
         )
+        activity.save()
+        activity.strength.set(sections)
         activity.save()
 
         return CreateActivity(
             activity_type=activity.activity_type,
             start_date=activity.start_date,
+            name=activity.name,
             description=activity.description,
             posted_by=activity.posted_by,
-            strength=activity.strength,
+            strength=sections,
         )
 
 class Mutation(graphene.ObjectType):
