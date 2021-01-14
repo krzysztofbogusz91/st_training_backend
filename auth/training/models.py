be86c8627769c29a78164ba10694f93296357d08
Labreche Abdellatif <tifo.zelo@gmail.com>
2021-01-14 15:01:00 +0100

    Using plural related names for foreign keys

--- auth/training/models.py
+++ auth/training/models.py
@@ -6,7 +6,7 @@ class TrainingSplit(models.Model):
     name = models.TextField(default="text", blank=True)
     description = models.TextField(blank=True)
     split_length = models.IntegerField(default=7, blank=True)
-    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, related_name='training_splits')
 
 
 class Training(models.Model):
@@ -14,7 +14,7 @@ class Training(models.Model):
     name = models.TextField(default="text", blank=True)
     day_in_split = models.IntegerField(null=True, blank=True)
     description = models.TextField(blank=True)
-    training_split = models.ForeignKey(TrainingSplit, related_name="training", null=True, blank=True, on_delete=models.CASCADE)
+    training_split = models.ForeignKey(TrainingSplit, related_name="trainings", null=True, blank=True, on_delete=models.CASCADE)
 
 
 class Exercise(models.Model):
@@ -27,7 +27,7 @@ class Exercise(models.Model):
     exercise_type = models.CharField(max_length=30, choices=TYPE_CHOICES, default=STRENGTH)
     exercise_name = models.CharField(max_length=100)
     body_part = models.CharField(max_length=100, null=True, blank=True)
-    training = models.ForeignKey(Training, related_name="exercise", null=True, blank=True, on_delete=models.CASCADE)
+    training = models.ForeignKey(Training, related_name="exercises", null=True, blank=True, on_delete=models.CASCADE)
 
 
 class ExerciseSet(models.Model):
@@ -36,4 +36,4 @@ class ExerciseSet(models.Model):
     duration = models.IntegerField(null=True, blank=True)
     distance = models.IntegerField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    exercise = models.ForeignKey(Exercise, related_name="exercise_set", null=True, blank=True, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, related_name="exercise_sets", null=True, blank=True, on_delete=models.CASCADE)
