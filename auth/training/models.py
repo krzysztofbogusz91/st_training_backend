821fcd136cbbfd18180e5d14fd67fe25bab98e0e
Labreche Abdellatif <tifo.zelo@gmail.com>
2021-01-14 15:00:00 +0100

    Removed unnecessary get_* methods from models

--- auth/training/models.py
+++ auth/training/models.py
@@ -1,24 +1,21 @@
 from django.conf import settings
 from django.db import models
 
+
 class TrainingSplit(models.Model):
     name = models.TextField(default="text", blank=True)
     description = models.TextField(blank=True)
     split_length = models.IntegerField(default=7, blank=True)
     posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
 
-    def get_training(self):
-        return self.training.all()
 
 class Training(models.Model):
     start_date = models.DateField()
     name = models.TextField(default="text", blank=True)
     day_in_split = models.IntegerField(null=True, blank=True)
     description = models.TextField(blank=True)
-    training_split = models.ForeignKey(TrainingSplit, related_name="training", null=True, blank = True, on_delete=models.CASCADE)
+    training_split = models.ForeignKey(TrainingSplit, related_name="training", null=True, blank=True, on_delete=models.CASCADE)
 
-    def get_exercises(self):
-        return self.exercise.all()
 
 class Exercise(models.Model):
     STRENGTH = "STRENGTH"
@@ -30,11 +27,8 @@ class Exercise(models.Model):
     exercise_type = models.CharField(max_length=30, choices=TYPE_CHOICES, default=STRENGTH)
     exercise_name = models.CharField(max_length=100)
     body_part = models.CharField(max_length=100, null=True, blank=True)
+    training = models.ForeignKey(Training, related_name="exercise", null=True, blank=True, on_delete=models.CASCADE)
 
-    training = models.ForeignKey(Training, related_name="exercise", null=True, blank = True, on_delete=models.CASCADE)
-    
-    def get_sets(self):
-        return self.exercise_set.all()
 
 class ExerciseSet(models.Model):
     weights = models.IntegerField(null=True, blank=True)
@@ -42,7 +36,4 @@ class ExerciseSet(models.Model):
     duration = models.IntegerField(null=True, blank=True)
     distance = models.IntegerField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-
-    exercise = models.ForeignKey(Exercise, related_name="exercise_set", null=True, blank = True, on_delete=models.CASCADE)
-
-
+    exercise = models.ForeignKey(Exercise, related_name="exercise_set", null=True, blank=True, on_delete=models.CASCADE)
